53ecb3d30c92ef56d7ca48d82063aa8f1d3a2a34
zoodogood <52154209+zoodogood@users.noreply.github.com>
2022-03-16 20:39:22 +0200

    Add files via upload

--- resources/modules/index/main.js
+++ resources/modules/index/main.js
@@ -11,4 +11,19 @@ await new FileLoader("resources/modules")
 
   new Header();
   new ScrollUp();
+
+  (async () => {
+    const articleParagraph = document.querySelector("#article__description .article__paragraph");
+    while (true){
+      await delay(4000 + random(10000));
+
+      const from = [...new Array(4)].map( () => random(1072, 1203) ).map(String.fromCharCode);
+      const glitch = new GlitchText(from, "Атом", { step: 2 });
+
+      for (const content of glitch){
+        await delay(15);
+        articleParagraph.textContent = content;
+      }
+    }
+  })()
 })();
